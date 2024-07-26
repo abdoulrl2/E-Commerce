95969fd8178e1c4a8bde1cae403748eef682461f
abdoulrl2 <abdoulrl2025@gmail.com>
2024-07-26 18:42:59 -0300

    Create ProductController.php

---
+++ Configuração do Banco de dados/database/migrations/database/migrations/database/migrations/database/migrations/ProductController.php
@@ -0,0 +1,13 @@
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+}
