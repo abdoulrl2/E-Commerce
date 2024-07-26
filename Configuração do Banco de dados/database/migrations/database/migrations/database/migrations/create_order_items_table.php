c618eff0bfac94630f70788e88c096d20303e8fd
abdoulrl2 <abdoulrl2025@gmail.com>
2024-07-26 18:39:26 -0300

    Create create_order_items_table.php

---
+++ Configuração do Banco de dados/database/migrations/database/migrations/database/migrations/create_order_items_table.php
@@ -0,0 +1,11 @@
+public function up()
+{
+    Schema::create('order_items', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('order_id')->constrained()->onDelete('cascade');
+        $table->foreignId('product_id')->constrained()->onDelete('cascade');
+        $table->integer('quantity');
+        $table->decimal('price', 8, 2);
+        $table->timestamps();
+    });
+}
